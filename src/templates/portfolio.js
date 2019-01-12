419f59408aa4e58c9bf5891cf73bcc3b305992a0
cyrusliew02 <cyrusliew02@gmail.com>
2019-01-13 01:28:42 +1300

    Fix template

---
+++ src/templates/portfolio.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const PortfolioTemplate = ({
+                                   content,
+                                   contentComponent,
+                                   description,
+                                   tags,
+                                   title,
+                                   helmet,
+                                 }) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+PortfolioTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const Portfolio = ({ data }) => {
+  const { markdownRemark: experience } = data;
+
+  return (
+    <Layout>
+      <PortfolioTemplate
+        content={experience.html}
+        contentComponent={HTMLContent}
+        description={experience.frontmatter.description}
+        helmet={
+          <Helmet
+            titleTemplate="%s | Blog"
+          >
+            <title>{`${experience.frontmatter.title}`}</title>
+            <meta name="description" content={`${experience.frontmatter.description}`} />
+          </Helmet>
+        }
+        tags={experience.frontmatter.tags}
+        title={experience.frontmatter.title}
+      />
+    </Layout>
+  )
+};
+
+Experience.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default Portfolio
+
+export const pageQuery = graphql`
+  query PortfolioByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`
